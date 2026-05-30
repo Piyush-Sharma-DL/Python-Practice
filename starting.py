78882ec2670e22b8a816977252827e1245c89897
Piyush-Sharma-DL <sharmapiyush9064@gmail.com>
2026-05-30 17:37:41 +0530

    Initial Python Practice

---
+++ starting.py
@@ -0,0 +1,12 @@
+Name = "Piyush Sharma"
+Age = 21
+Price = 999.99
+print(Name,Age,Price)
+print(type(Name))
+print(type(Age))
+print(type(Price))
+X = Name
+print(X)
+print("Name:" ,Name)
+print("Age:",Age)
+print("Price:",Price)
