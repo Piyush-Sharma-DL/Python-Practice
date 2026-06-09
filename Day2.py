99b1382ef514c3d82a2908afe8390fd14fa60542
Piyush-Sharma-DL <sharmapiyush9064@gmail.com>
2026-06-09 16:59:02 +0530

    variables and data types

---
+++ Day2.py
@@ -0,0 +1,9 @@
+
+print("Hey its 5th june\nIam starting my journey of laerning python")
+print("This year IPL won by RCB on",31,5,2026,sep=".",end="Red & gold")
+
+" "
+#IDE:-PYCHARM
+#PROGRAMER:- PIYUSH
+#DATE:- 5/06/2026
+" "
---
+++ variables and data types.py
@@ -0,0 +1,9 @@
+a = complex(10,9)
+print(type(a))
+print(a)
+b=10.80
+print(type(b))
+print(b)
+c=True
+print(type(c))
+print(c)
